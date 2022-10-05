0a34793c3b40358f09b888e366f41e26fea8dc75
akwalton <andrew.walton@student.unimelb.edu.au>
2022-10-05 13:02:19 +1100

    Added time scale attributes and started level abstract class

--- src/ShadowDimension.java
+++ src/ShadowDimension.java
@@ -11,7 +11,21 @@ public class ShadowDimension extends AbstractGame {
     private final static int WINDOW_WIDTH = 1024;
     private final static int WINDOW_HEIGHT = 768;
     private final static String GAME_TITLE = "SHADOW DIMENSION";
-    private final Image BACKGROUND_IMAGE = new Image("res/background0.png");
+
+    /** Time Scale attributes */
+    private double timeScale;
+    private final double TIME_SCALE_CHANGE_PERCENT = 50;
+    private final double MAX_TIME_SCALE = 3;
+    private final double MIN_TIME_SCALE = -3;
+
+    /** Font attributes */
+    private static final String FONT_PATH = "res/frostbite.ttf";
+    private static final int DEFAULT_FONT_SIZE = 75;
+    private static final Font DEFAULT_FONT = new Font(FONT_PATH, DEFAULT_FONT_SIZE);
+    
+    private static final int REFRESH_RATE = 60;
+
+
 
     public ShadowDimension(){
         super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
@@ -39,7 +53,6 @@ public class ShadowDimension extends AbstractGame {
      */
     @Override
     protected void update(Input input) {
-        BACKGROUND_IMAGE.draw(Window.getWidth()/2.0, Window.getHeight()/2.0);
 
         if (input.wasPressed(Keys.ESCAPE)){
             Window.close();
